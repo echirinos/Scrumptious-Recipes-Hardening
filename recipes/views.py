f2c05fd3d4ac4d183fa5581203b5171e64970dc7
esteban <esteban.chirinos@gmail.com>
2022-08-30 22:38:47 -0400

    added authors to recipes on create and edit

--- recipes/views.py
+++ recipes/views.py
@@ -42,7 +42,7 @@ class RecipeDetailView(DetailView):
 class RecipeCreateView(CreateView):
     model = Recipe
     template_name = "recipes/new.html"
-    fields = ["name", "author", "description", "image"]
+    fields = ["name", "description", "image"]
     success_url = reverse_lazy("recipes_list")
 
 
@@ -62,5 +62,9 @@ class RecipeDeleteView(DeleteView):
 class RecipeCreateView(LoginRequiredMixin, CreateView):
     model = Recipe
     template_name = "recipes/new.html"
-    fields = ["name", "author", "description", "image"]
+    fields = ["name", "description", "image"]
     success_url = reverse_lazy("recipes_list")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
